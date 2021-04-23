bdcc62a0af1ade1c8e7dfc83b1fadcc4aff688c7
Lee Hin Tak <LeeHinTak@gmail.com>
2021-04-23 21:55:07 +0800

    Reset, shift to top

--- App.js
+++ App.js
@@ -1,17 +1,63 @@
 import { StatusBar } from "expo-status-bar";
+import {useState} from 'react'; 
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import BlockRGB from "./components/BlockRGB";
 
 function HomeScreen() {
+  const [colorArray, setColorArray] = useState([
+    { red: 255, green: 0, blue: 0, id: "0"},
+    { red: 0, green: 255, blue: 0, id: "1"},
+    { red: 0, green: 0, blue: 255, id: "2"},
+  ]);
+
+  function addColor() {
+    setColorArray([
+      {
+        red: Math.floor(Math.random() * 256),
+        green: Math.floor(Math.random() * 256),
+        blue: Math.floor(Math.random() * 256),
+        id: `${colorArray.length}`,
+      },
+      ...colorArray,
+
+    ]);
+  }
+  
+  function resetColor() {
+    setColorArray([]);
+  }
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity
+        style={{ height: 40, justifyContent: "center" }}
+        onPress={addColor}
+      >
+        <Text style={{ color: "red" }}>Add colour</Text>
+        
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={{ height: 40, justifyContent: "center" }}
+        onPress={resetColor}
+      >
+        <Text style={{ color: "green" }}>Delete colour</Text>
+        
+      </TouchableOpacity>
+      <FlatList style={styles.list} data={colorArray} renderItem={renderItem} />
+    </View>
+  )
+ 
+  function renderItem({ item}) {
+    return <BlockRGB red={item.red} green={item.green} blue={item.blue} />;
+  }
+
   return (
 
       <View style={styles.container}>
-        <BlockRGB red={255} green={0} blue={0} />
-        <BlockRGB red={0} green={255} blue={0} />
-        <BlockRGB red={0} green={0} blue={255} />
+        <FlatList style={styles.list} data={colorArray} renderItem={renderItem} />
       </View>
       
   );
@@ -33,7 +79,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
   },
 });
